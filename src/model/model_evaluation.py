9430b0add80f30b4b7c9592b4d148d96c32cbf6a
ajit <khandaleajit@gmail.com>
2024-08-31 00:22:02 +0530

    add token based auth for dagshub

--- src/model/model_evaluation.py
+++ src/model/model_evaluation.py
@@ -20,8 +20,8 @@ os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
 os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
 dagshub_url = "https://dagshub.com"
-repo_owner = "campusx-official"
-repo_name = "mlops-mini-project"
+repo_owner = "ajit1393"
+repo_name = "end-to-end-mlops-mini-project"
 
 # Set up MLflow tracking URI
 mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
